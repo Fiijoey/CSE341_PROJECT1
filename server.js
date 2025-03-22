7d8cd910f0316933efa3063487a0ad2f55100ea6
Fiijoey <kofimensah3730@gmail.com>
2025-03-22 21:52:21 +0000

    initialize project with Express, MongoDB, and basic routing

---
+++ server.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+
+const mongodb = require("./dB/db");
+
+const port = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Z-Key"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, OPTIONS"
+  );
+  next();
+});
+app.use("/", require("./routes"));
+
+// Error handling
+process.on("uncaughtException", (err, Origin) => {
+  console.log(
+    process.stderr.fd,
+    `Caught exception: ${err}\n` + `Exception origin: ${Origin}`
+  );
+});
+
+mongodb.initDb((err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    app.listen(port, () => {
+      console.log(`Server is running on port localhost:${port}`);
+    });
+  }
+});
